16efd815e77d7508a81108af06166f1e911aadfd
DouglasDDo <dougddo@gmail.com>
2017-05-08 11:59:39 -0700

    Tries using multiple return items

--- the-hard-way/ex_21.py
+++ the-hard-way/ex_21.py
@@ -18,8 +18,16 @@ def mod(a, b):
     print "Modding %d by %d" % (a, b)
     return a % b
 
+# NOTE: returning multiple items results in a List
+def lotta_math(a, b):
+    print "Doing a whole lotta math:"
+    return a % b, (a + b), (a - b)
+
 print add(1,2)
 print subtract(5,3)
 print multiply(2,3)
 print divide(10,5)
 print mod(13,5)
+print lotta_math(13, 5)
+foo = lotta_math(13, 5)
+print foo[0]

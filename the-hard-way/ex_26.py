c7b6ae8229a55b9e9616c0b7fcc78fd5cbe4a772
DouglasDDo <dougddo@gmail.com>
2017-05-08 14:28:17 -0700

    Completes debugging test

--- the-hard-way/ex_26.py
+++ the-hard-way/ex_26.py
@@ -8,16 +8,16 @@ def sort_words(words):
     return sorted(words)
 
 # Missing colon
-def print_first_word(words)
+def print_first_word(words):
     """Prints the first word after popping it off."""
     # Misspelled method
-    word = words.poop(0)
+    word = words.pop(0)
     print word
 
 def print_last_word(words):
     """Prints the last word after popping it off."""
     # Missing parentheses
-    word = words.pop(-1
+    word = words.pop(-1)
     print word
 
 def sort_sentence(sentence):
@@ -55,49 +55,50 @@ print "--------------"
 print poem
 print "--------------"
 
-five = 10 - 2 + 3 - 5
+five = 10 - 2 + 3 - 4
 # Uses string instead of digit
-print "This should be five: %s" % five
+print "This should be five: %d" % five
 
 def secret_formula(started):
     jelly_beans = started * 500
     # Uses escape instead of division
-    jars = jelly_beans \ 1000
+    jars = jelly_beans / 1000
     crates = jars / 100
     return jelly_beans, jars, crates
 
 
 start_point = 10000
 # Uses comparison instead of assignment
-beans, jars, crates == secret_formula(start-point)
+beans, jars, crates = secret_formula(start_point)
 
 print "With a starting point of: %d" % start_point
-print "We'd have %d jeans, %d jars, and %d crates." % (beans, jars, crates)
+print "We'd have %d jelly beans, %d jars, and %d crates." % (beans, jars, crates)
 
+# Could shorten to start_point /= 10; not necessarily incorrect though
 start_point = start_point / 10
 
 print "We can also do that this way:"
+print "With a starting point of: %d" % start_point
 # Misspells 'point', missing parentheses
-print "We'd have %d beans, %d jars, and %d crabapples." % secret_formula(start_pont
+print "We'd have %d jelly beans, %d jars, and %d crates." % secret_formula(start_point)
 
 
 # Misspellings in str
-sentence = "All god\tthings come to those who weight."
+sentence = "All good\tthings come to those who wait."
 
 # Does not import ex25, undefined
-words = ex25.break_words(sentence)
-sorted_words = ex25.sort_words(words)
+words = break_words(sentence)
+sorted_words = sort_words(words)
 
 print_first_word(words)
 print_last_word(words)
 # Erroeneous period
-.print_first_word(sorted_words)
+print_first_word(sorted_words)
 print_last_word(sorted_words)
-sorted_words = ex25.sort_sentence(sentence)
+sorted_words = sort_sentence(sentence)
 # Misspelled 'print'
-prin sorted_words
+print sorted_words
 
 # Misspelled method
-print_irst_and_last(sentence)
-
-   print_first_a_last_sorted(senence)
+print_first_and_last(sentence)
+print_first_and_last_sorted(sentence)

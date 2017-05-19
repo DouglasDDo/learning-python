9bbbd86f5c9feee9960899ab46e6cff502f99159
DouglasDDo <dougddo@gmail.com>
2017-05-19 14:18:51 -0700

    Adds other points for Py2

---
+++ the-hard-way/other_points.py
@@ -0,0 +1,12 @@
+# NOTE: Classes can also be used like Structs
+
+class StructLike:
+    pass
+
+foo = StructLike()
+foo.detail = "Some words"
+foo.num = 123
+foo.more_details = "Some more words"
+
+print foo
+print foo.detail
